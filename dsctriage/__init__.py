2cfb9bc23a8ad10abef122394280fa4b4f728c0b
Lena Voytek <lena.voytek@canonical.com>
2022-07-15 14:40:29 -0700

    Remove unnecessary imports in __init__.py

--- dsctriage/__init__.py
+++ dsctriage/__init__.py
@@ -2,8 +2,6 @@
 from dsctriage.discourse_post import DiscoursePost
 from dsctriage.discourse_topic import DiscourseTopic
 from dsctriage.discourse_category import DiscourseCategory
-from dsctriage import dscfinder
-from dsctriage import dsctriage
 
 __all__ = [
     'DiscoursePost', 'DiscourseTopic', 'DiscourseCategory'
